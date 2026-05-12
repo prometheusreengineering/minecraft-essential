40a2a3680b8c929c401d2f77c1fd6500bffbffa7
henry <henry@cootshk.dev>
2026-05-11 20:23:29 -0500

    move the folder structure around a bit + no json wrapping

--- src/main/kotlin/studio/dreamys/prometheus/serial/EssentialCosmeticsManager.kt
+++ src/main/kotlin/studio/dreamys/prometheus/serial/EssentialCosmeticsManager.kt
@@ -4,30 +4,28 @@ import kotlinx.serialization.json.Json
 import studio.dreamys.prometheus.util.OS
 import java.io.File
 import java.io.FileOutputStream
+import java.net.URI
 import java.util.logging.Logger
-import kotlin.io.path.Path
-import kotlin.io.path.absolutePathString
-import kotlin.io.path.createDirectories
-import kotlin.io.path.createSymbolicLinkPointingTo
-import kotlin.io.path.exists
+import kotlin.concurrent.thread
+import kotlin.io.path.*
 
 object EssentialCosmeticsManager {
     private val logger: Logger = Logger.getLogger("Prometheus")
     private val cosmeticsData: EssentialCosmeticsData
 
     @JvmField
-    val PROMETHEUS_FOLDER = Path("prometheus")
+    val PROMETHEUS_FOLDER = Path("prometheus", "essential")
     private val PROMETHEUS_GLOBAL_FOLDER = when (OS.current) {
         OS.Windows -> PROMETHEUS_FOLDER // No global folder on Windows because of the lack of true symlinks
-        OS.Linux -> Path(System.getenv("XDG_DATA_HOME"), "prometheus")
-        OS.MacOS -> Path(System.getProperty("user.home"), "Library", "Application Support", "prometheus");
+        OS.Linux -> Path(System.getenv("XDG_DATA_HOME"), "prometheus", "essential")
+        OS.MacOS -> Path(System.getProperty("user.home"), "Library", "Application Support", "prometheus", "essential");
     }
 
     @JvmField
-    val DUMPS_PATH = Path(PROMETHEUS_FOLDER.absolutePathString(), "dumps/essential")
+    val DUMPS_PATH = Path(PROMETHEUS_FOLDER.absolutePathString(), "dumps")
 
     @JvmField
-    val COSMETICS_FILE = File(PROMETHEUS_FOLDER.toFile(), "essential.json")
+    val COSMETICS_FILE = File(PROMETHEUS_FOLDER.toFile(), "cosmetics.json")
 
     private fun loadCosmeticsFile() {
         if (!COSMETICS_FILE.exists()) {
@@ -61,15 +59,38 @@ object EssentialCosmeticsManager {
 
     @JvmStatic
     fun downloadCosmeticsList() {
-        // TODO: Fetch from GitHub
-        // no TODO() because of the `init {}` block.
+       thread {
+           val body: String
+           try {
+               // CHANGE ME BEFORE MERGING
+               val inputStream = URI("https://github.com/prometheusreengineering/minecraft-essential/raw/refs/heads/feat/legacy-list/src/main/resources/cosmetics.json").toURL().openStream()
+               body = inputStream.reader().readText()
+           } catch (_: Exception) {
+               logger.warning("Failed to download new cosmetics!")
+               return@thread
+           }
+           try {
+               logger.info("Body text: \n$body")
+               var count = 0
+               Json.decodeFromString<EssentialCosmeticsData>(body).legacyCosmetics.forEach { id ->
+                   if (addCosmetic(id, false)) count++
+               }
+               saveCosmetics()
+               logger.info("Merged $count cosmetics!")
+           } catch (e: Exception) {
+               logger.warning("Failed to download new cosmetics!")
+               e.printStackTrace()
+           }
+       }
     }
 
     @JvmStatic
-    fun addCosmetic(id: String): Boolean {
+    fun addCosmetic(id: String)
+        = addCosmetic(id, true)
+    private fun addCosmetic(id: String, save: Boolean): Boolean {
         if (id in cosmeticsData.legacyCosmetics) return false;
         cosmeticsData.legacyCosmetics += id
-        saveCosmetics()
+        if (save) saveCosmetics()
         return true;
     }
 
