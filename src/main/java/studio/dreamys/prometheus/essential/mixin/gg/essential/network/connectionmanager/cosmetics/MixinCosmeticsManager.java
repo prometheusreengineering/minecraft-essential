df3549c0a5be931c5a1b0fce5e2ea27de5596967
Henry <henry@cootshk.dev>
2026-05-16 23:31:29 -0500

    fix: this loads and then copies

--- src/main/java/studio/dreamys/prometheus/essential/mixin/gg/essential/network/connectionmanager/cosmetics/MixinCosmeticsManager.java
+++ src/main/java/studio/dreamys/prometheus/essential/mixin/gg/essential/network/connectionmanager/cosmetics/MixinCosmeticsManager.java
@@ -12,7 +12,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import studio.dreamys.prometheus.essential.serial.EssentialCosmeticsFileData;
-import studio.dreamys.prometheus.essential.serial.EssentialCosmeticsManager;
 
 @Mixin(value = CosmeticsManager.class, remap = false)
 public abstract class MixinCosmeticsManager {
